3a054639fdd50739e40cdb22f325ccf72828a3af
Alec Ikawa <72089401+alecikawa@users.noreply.github.com>
2026-01-07 10:18:59 -0300

    desafio finalizado

--- src/script.js
+++ src/script.js
@@ -1,5 +1,41 @@
-console.log("Digite o nome do seu jogador")
+let nomeJogador = "ikawaFPS";
+let xpJogador = -100;
+let rank;
 
-let nickname = "Ikawa"
+switch (true) {
+    case xpJogador >= 0 && xpJogador <= 1000:
+        rank = "Ferro";
+        break;
 
-console.log("Bem vindo, " + nickname + "!")
+    case xpJogador >= 1001 && xpJogador <= 2000:
+            rank = "Bronze";
+            break;
+
+    case xpJogador >= 2001 && xpJogador <= 5000 :
+        rank = "Prata";
+        break;
+
+    case xpJogador >= 5001 && xpJogador <= 7000:
+        rank = "Ouro";
+        break;
+
+    case xpJogador >= 7001 && xpJogador <= 8000:
+        rank = "Platina";
+        break;
+    case xpJogador >= 8001 && xpJogador <= 9000:
+         rank = "Ascendente";
+        break;
+    case xpJogador >= 9001 && xpJogador <= 10000:
+        rank = "Imortal";
+        break;
+    case xpJogador >= 10001:
+        rank = "Radiante";
+        break;
+
+    default:
+        rank = "Rank Inválido";
+        break;
+        
+}
+
+console.log("O Herói de nome " + nomeJogador + " está no nível de " + rank);
